9ebd6d6e59ab5e85657b3d30d5cc8d2b5ca9c22e
Conrado P. L. Gouvea <conradoplg@gmail.com>
2014-06-01 14:15:12 -0300

    Sort part window by color usage

--- desktop/src/net/cryptoland/bricksearch/desktop/PartWindow.java
+++ desktop/src/net/cryptoland/bricksearch/desktop/PartWindow.java
@@ -8,6 +8,8 @@ import javax.swing.event.HyperlinkListener;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -52,13 +54,25 @@ public class PartWindow implements IImageViewer {
 
         String ownershipText = "<html>";
         Map<String, List<SetPart>> ownershipData = userPartDB.getPartOwnershipData(id);
-        for (String colorID: ownershipData.keySet()) {
+        for (String colorID: database.getSortedColorIDs()) {
             int count = 0;
             String colorText = "<ul>";
-            for (SetPart setPart: ownershipData.get(colorID)) {
+            List<SetPart> setPartList = ownershipData.get(colorID);
+            if (setPartList == null) {
+                continue;
+            }
+            Collections.sort(setPartList, new Comparator<SetPart>() {
+                @Override
+                public int compare(SetPart o1, SetPart o2) {
+                    return o2.getQuantity() - o1.getQuantity();
+                }
+            });
+            for (SetPart setPart: setPartList) {
                 Set set = database.getSetDB().getSet(setPart.getSetID());
                 count += setPart.getQuantity();
-                colorText += "<li>" + setPart.getQuantity() + " in <a href=\"http://brickset.com/sets/" + setPart.getSetID() + "\">" + setPart.getSetID() + "</a> " + set.getName() + "</li>";
+                colorText += "<li>" + setPart.getQuantity() + " in <a href=\"http://brickset.com/sets/"
+                        + setPart.getSetID() + "\">" + setPart.getSetID() + "</a> "
+                        + (set == null ? "" : set.getName()) + "</li>";
             }
             colorText += "</ul>";
             ownershipText += "<p>" + count + " in " + colorDB.getDescription(colorID) + "</p>";
